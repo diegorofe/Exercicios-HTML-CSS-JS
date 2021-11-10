5592cf29c0b07c9df701a9b52511dfa5364e61b3
Diego <diegorf55@gmail.com>
2021-11-10 20:57:44 -0300

    exercise send DOM for to function

---
+++ Javascript/exercicio17/index.js
@@ -0,0 +1,9 @@
+function saveMyFirstName(element){
+    alert("meu primeiro nome é " + element.value)
+}
+
+function saveMyLastName(){
+   
+    console.log(event)
+    alert("Meu sobrenome é " + event.target.value)
+}
