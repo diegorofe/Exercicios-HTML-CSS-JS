745b6a69bcc91a105810777bf7d3d58d7d4905ef
Diego <diegorf55@gmail.com>
2021-11-20 23:46:42 -0300

    increment then() and catch()

--- Javascript/exercicio30/src/index.js
+++ Javascript/exercicio30/src/index.js
@@ -1,13 +1,13 @@
 let komodoShip = {
     name: "Komodo",
     velocity: 80,
-    acceleration: 10
+    acceleration: 0
 }
 
-const velocityAfter2Seconds(velocity, acceleration){
+const velocityAfter2Seconds = (velocity, acceleration) =>{
     
-    new Promise(function(resolve, reject) {
-        setTimeout(() => {
+   return new Promise(function(resolve, reject) {
+       setTimeout(() => {
             if(acceleration > 0){
                 velocity += acceleration * 2
                 console.log(`Nova velocidade: ${velocity}`)
@@ -17,10 +17,16 @@ const velocityAfter2Seconds(velocity, acceleration){
                 reject("Não possui aceleração")
 
             }
-        }, 2000)    
+       }, 2000)    
 })
 }
 
-velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration)
+velocityAfter2Seconds(komodoShip.velocity, komodoShip.acceleration).then((velocity) => {
+    komodoShip.velocity = velocity
+    console.log("Depois de acelerar\n", komodoShip)
+}).catch(message =>{
+    console.log(`Komodo: ${message}`)
+})
 
 console.log("Execução assíncrona Promise")
+console.log(komodoShip)
